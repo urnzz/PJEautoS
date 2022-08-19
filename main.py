2b0bf9d5b0c02afedaa36742439022f2bec45911
urnz <rafaelsansana25@hotmail.com>
2022-08-19 19:56:55 -0300

    minor fixes

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ def main():
     f = open("cpfs.txt", "r")
     cpfs = f.readlines()
     o = open('output.txt', 'w+')
+    options = uc.ChromeOptions()
+    options.headless=True
+    driver = uc.Chrome(options=options)
     for cpf in cpfs:
         try:
-            options = uc.ChromeOptions()
-            options.headless=True
-            options.add_argument('--headless')
-            driver = uc.Chrome(options=options)
-            print("pesquisando "+cpf)
+            print("pesquisando no PJE "+cpf.strip())
             driver.get("https://www.google.com/search?client=firefox-b-d&q=consulta+pje")
             WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[7]/div/div[10]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/a/h3")))
             gs = driver.find_element(By.XPATH, "/html/body/div[7]/div/div[10]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/a/h3")
@@ -22,24 +21,30 @@ def main():
             cpfC = driver.find_element(By.XPATH, '//*[@id="fPP:dpDec:documentoParte"]')
             cpfC.click()
             cpfC.clear()
-            cpfC.send_keys(cpf)
-            #driver.find_element(By.XPATH, '//*[@id="fPP:searchProcessos"]').click()
+            cpfC.send_keys(cpf.strip())
+            driver.find_element(By.XPATH, '//*[@id="fPP:searchProcessos"]').click()
             try:
                 WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[5]/div/div/div/div[2]/form/div[2]/div/table/tbody/tr')))
                 t=driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div/div[2]/form/div[2]/div/table/tbody')
                 rows = t.find_elements(By.TAG_NAME, "tr")
                 for row in rows:
-                    if "teto" or "Teto" in row.find_element(By.CLASS_NAME, "btn-block").text:
+                    if "teto" in str(row.find_element(By.CLASS_NAME, "btn-block").text):
                         print('encontrado')
-                        o.write(cpf+","+row.find_element(By.class_name, "btn-block").text)
+                        o.write(cpf.strip()+","+row.find_element(By.CLASS_NAME, "btn-block").text+"\n")
                         break
                     else:
-                        print('sem processo de teto')
-                        o.write(cpf+",sem processo de teto encontrado")
+                        if "Teto" in str(row.find_element(By.CLASS_NAME, "btn-block").text):
+                            print('encontrado')
+                            o.write(cpf.strip()+","+row.find_element(By.CLASS_NAME, "btn-block").text+"\n")
+                            break
+                        else:
+                            print('processo encontrado porém não é teto')
+                            o.write(cpf.strip()+",\n")
             except:
+                o.write(cpf.strip()+",\n")
                 print("sem processo de teto") 
-            driver.close()
         except:
+            o.write(cpf.strip()+",erro\n")
             print('erro encontrado, pulando cpf: '+cpf)
             pass
 
