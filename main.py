e1b17ab4f1bae9615a9e0f83ca6d692f89c8158f
urnz <rafaelsansana25@hotmail.com>
2022-09-01 12:27:28 -0300

    wip

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ def main():
     cpfs = f.readlines()
     o = open('output.txt', 'w+')
     options = uc.ChromeOptions()
-    options.headless=True
+    #options.headless=True
     driver = uc.Chrome(options=options)
     for cpf in cpfs:
         try:
             print("pesquisando no PJE "+cpf.strip())
             driver.get("https://www.google.com/search?client=firefox-b-d&q=consulta+pje")
-            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[7]/div/div[10]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/a/h3")))
-            gs = driver.find_element(By.XPATH, "/html/body/div[7]/div/div[10]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/a/h3")
+            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="rso"]/div[1]/div/div/div[1]/div/a/h3')))
+            gs = driver.find_element(By.XPATH, '//*[@id="rso"]/div[1]/div/div/div[1]/div/a/h3')
             gs.click()
             WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="fPP:dpDec:documentoParte"]')))
             cpfC = driver.find_element(By.XPATH, '//*[@id="fPP:dpDec:documentoParte"]')
@@ -24,7 +24,7 @@ def main():
             cpfC.send_keys(cpf.strip())
             driver.find_element(By.XPATH, '//*[@id="fPP:searchProcessos"]').click()
             try:
-                WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[5]/div/div/div/div[2]/form/div[2]/div/table/tbody/tr')))
+                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '/html/body/div[5]/div/div/div/div[2]/form/div[2]/div/table/tbody/tr')))
                 t=driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div/div[2]/form/div[2]/div/table/tbody')
                 rows = t.find_elements(By.TAG_NAME, "tr")
                 for row in rows:
@@ -40,6 +40,7 @@ def main():
                         else:
                             raise Exception('') 
             except:
+                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//html/body/div[5]/div/div/div/div[2]/form/div[2]/div/dl/dt/span')))
                 o.write(cpf.strip()+",\n")
                 print("sem processo de teto") 
         except:
